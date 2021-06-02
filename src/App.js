283c0c7f29706889f2dd3716fdab5bd4e4b74ae3
jjayjack <jocelynjayjack@gmail.com>
2021-06-02 18:58:14 -0500

    Updated App to include separate container and head

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import Header from './components/Header'
 import EmployeeTableContainer from "./components/EmployeeTableContainer";
 
+
+
 export default function App() {
-    return <EmployeeTableContainer />
+    return (
+    <>
+    <Header />    
+    <EmployeeTableContainer />
+    </>
+    )
 }
