8412d920e93cc601827bbaad8a427b00aba9a3b8
jjayjack <jocelynjayjack@gmail.com>
2021-06-02 19:00:13 -0500

    Updated Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="jumbotron jumbotron-fluid center">
+            <div className="container">
+                <h1 className="display-4">Random Workplace for You</h1>
+                <p className="lead">Employee Directory</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
